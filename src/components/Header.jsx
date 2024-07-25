4832ab147d5db4a40dbb36b40ff4ad68feb3b69e
Florian-Godeau <flgodeau@gmail.com>
2024-07-25 21:44:34 +0200

    correction navbar collapse

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,27 +17,29 @@ function Header() {
           <img src={logo} alt="Logo" className="header__logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setOpen(!open)} aria-expanded={open} />
-        <Collapse in={open}>
-          <div id="basic-navbar-nav">
-            <Nav className="header__nav">
-              <Nav.Link as={Link} to="/" className={`header__nav-link ${location.pathname === '/' ? 'header__nav-link--active' : ''}`}>Accueil</Nav.Link>
-              <Nav.Link as={Link} to="/about" className={`header__nav-link ${location.pathname === '/about' ? 'header__nav-link--active' : ''}`}>Qui suis-je ?</Nav.Link>
-              <Nav.Link as={Link} to="/price" className={`header__nav-link ${location.pathname === '/price' ? 'header__nav-link--active' : ''}`}>Tarifs</Nav.Link>
-              <Nav.Link as={Link} to="/contact" className={`header__nav-link ${location.pathname === '/contact' ? 'header__nav-link--active' : ''}`}>Contact</Nav.Link>
-            </Nav>
-            <Nav className="header__social">
-              <Nav.Link href="https://www.instagram.com/lucieplumeria/" target="_blank" className="header__social-link">
-                <img src={instagramLogo} alt="Instagram" className="header__social-logo" />
-              </Nav.Link>
-              <Nav.Link href="https://www.linkedin.com/in/lucie-plumeria/" target="_blank" className="header__social-link">
-                <img src={linkedinLogo} alt="LinkedIn" className="header__social-logo" />
-              </Nav.Link>
-              <Nav.Link href="https://www.youtube.com/@LuciePlumeria" target="_blank" className="header__social-link">
-                <img src={youtube} alt="Youtube" className="header__social-logo" />
-              </Nav.Link>
-            </Nav>
-          </div>
-        </Collapse>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Collapse in={open}>
+            <div className="navbar-collapse">
+              <Nav className="header__nav">
+                <Nav.Link as={Link} to="/" className={`header__nav-link ${location.pathname === '/' ? 'header__nav-link--active' : ''}`}>Accueil</Nav.Link>
+                <Nav.Link as={Link} to="/about" className={`header__nav-link ${location.pathname === '/about' ? 'header__nav-link--active' : ''}`}>Qui suis-je ?</Nav.Link>
+                <Nav.Link as={Link} to="/price" className={`header__nav-link ${location.pathname === '/price' ? 'header__nav-link--active' : ''}`}>Tarifs</Nav.Link>
+                <Nav.Link as={Link} to="/contact" className={`header__nav-link ${location.pathname === '/contact' ? 'header__nav-link--active' : ''}`}>Contact</Nav.Link>
+              </Nav>
+              <Nav className="header__social">
+                <Nav.Link href="https://www.instagram.com/lucieplumeria/" target="_blank" className="header__social-link">
+                  <img src={instagramLogo} alt="Instagram" className="header__social-logo" />
+                </Nav.Link>
+                <Nav.Link href="https://www.linkedin.com/in/lucie-plumeria/" target="_blank" className="header__social-link">
+                  <img src={linkedinLogo} alt="LinkedIn" className="header__social-logo" />
+                </Nav.Link>
+                <Nav.Link href="https://www.youtube.com/@LuciePlumeria" target="_blank" className="header__social-link">
+                  <img src={youtube} alt="Youtube" className="header__social-logo" />
+                </Nav.Link>
+              </Nav>
+            </div>
+          </Collapse>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
